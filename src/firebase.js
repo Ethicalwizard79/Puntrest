69f27b9d0c73735ecd17dca2d049356867af26c1
Prasann <prasana9503@gmail.com>
2024-09-24 20:25:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA44CAURxPGr4kYn2jrxqpZJy4PrCB8OLA",
-  authDomain: "fir-login-343fc.firebaseapp.com",
-  databaseURL: "https://fir-login-343fc-default-rtdb.firebaseio.com",
-  projectId: "fir-login-343fc",
-  storageBucket: "fir-login-343fc.appspot.com",
-  messagingSenderId: "210013598411",
-  appId: "1:210013598411:web:01090583d1cfb0a072214e"
+  your config details
 };
 
 
